a7dffb3d3b28a6aec864a0688ec6f8720ad1fd68
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-12 01:06:47 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,11 @@
 <?php
 require_once('rb.php');
-$dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = Insurance";
 
+$dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = Insurance";
 $login = "Romanow";
 $pass = "Qwerty123456";
-try {
-  R::setup($dsn, $login, $pass);
-}
-catch (Exception $ex) {
-  echo 'Не связанный '.$ex->getMessage();
-}
+
+R::setup($dsn, $login, $pass);
 
 session_start();
  ?>
