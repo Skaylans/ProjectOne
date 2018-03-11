e8392f5798c4ca03f692d2e2f110a0c94f6021ed
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-12 01:09:32 +0400

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,15 @@ $dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = Insur
 $login = "Romanow";
 $pass = "Qwerty123456";
 
-R::setup($dsn, $login, $pass);
+//R::setup($dsn, $login, $pass);
+
+try {
+    $conn = new PDO($dsn, $login, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $ex) {
+    echo 'Не связанный '.$ex->getMessage();
+}
 
 session_start();
  ?>
