683e5b944027fd87f5661e4ad3b14fb9c9e0c5b6
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-20 11:25:02 +0300

    Update basis-policy.php

--- basis-policy.php
+++ basis-policy.php
@@ -1,3 +1,57 @@
+<?php      
+require_once('db.php');
+
+if (isset($_POST['payment'])) {
+  
+  $phonenum = $_POST['user_phone'];
+  $insurant_email = $_POST['insurant_email'];
+  
+  $insurant_lastname = $_POST['insurant_lastname'];
+  $insurant_name = $_POST['insurant_name'];
+  $insurant_middlename = $_POST['insurant_middlename'];
+  
+  $gender = $_POST['gender'];
+  $insurant_birthdate = $_POST['insurant_birthdate'];
+  
+  $insurant_psn = $_POST['insurant_psn'];
+  $issued_by = $_POST['issued_by'];
+  $date_issue = $_POST['date_issue'];
+  
+  $insurant_city_reg = $_POST['insurant_city_reg'];
+  $insurant_street_reg = $_POST['insurant_street_reg'];
+  $insurant_house_reg = $_POST['insurant_house_reg'];
+  $insurant_room_reg = $_POST['insurant_room_reg'];
+  
+  $insurant_city_res = $_POST['insurant_city_res'];
+  $insurant_street_res = $_POST['insurant_street_res'];
+  $insurant_house_res = $_POST['insurant_house_res'];
+  $insurant_room_res = $_POST['insurant_room_res'];
+  
+  $address_include = $_POST['address_include'];
+  $insured_include = $_POST['insured_include'];
+  
+  $insured_lastname = $_POST['insured_lastname'];
+  $insured_name = $_POST['insured_name'];
+  $insured_middlename = $_POST['insured_middlename']
+  $insured_birthdate = $_POST['insured_birthdate'];
+      
+}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -21,22 +75,22 @@
 
             <div class="form-group" style="float: left; margin-left: 50px;">
               <label for="">E-mail</label>
-              <input type="text" class="form-control" id="" name="user_email" placeholder="test@mail.ru">
+              <input type="text" class="form-control" id="" name="insurant_email" placeholder="test@mail.ru">
             </div>
 
             <div class="form-group">
       				<label for="">Фамилия</label>
-      				<input type="text" class="form-control" id="" name="user_lastname" placeholder="Иванов" style="width: 650px;">
+      				<input type="text" class="form-control" id="" name="insurant_lastname" placeholder="Иванов" style="width: 650px;">
       			</div>
 
             <div class="form-group">
       				<label for="">Имя</label>
-      				<input type="text" class="form-control" id="" name="user_name" placeholder="Иван" style="width: 650px;">
+      				<input type="text" class="form-control" id="" name="insurant_name" placeholder="Иван" style="width: 650px;">
       			</div>
 
             <div class="form-group">
               <label for="">Отчество</label>
-              <input type="text" class="form-control" id="" name="user_middlename" placeholder="Иванович" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insurant_middlename" placeholder="Иванович" style="width: 650px;">
             </div>
 
             <label><h3>Ваш пол</h3></label>
@@ -46,7 +100,7 @@
             <div class="form-group" style="float: left;">
               <label for="" style="font-size: 1.4em;">
                 <span>
-                  <input type="radio" class="control" id="" name="user_gender" value="Man" style="width: 25px;
+                  <input type="radio" class="control" id="" name="gender" value="Man" style="width: 25px;
                 height: 25px;"></span>
                 Мужчина
               </label>
@@ -54,7 +108,7 @@
 
             <div class="form-group"  style="float: left; margin-left: 50px;">
               <label for="" style="font-size: 1.4em;">
-                <span><input type="radio" class="control" id="" name="user_gender" value="Woman" style="width: 25px;
+                <span><input type="radio" class="control" id="" name="gender" value="Woman" style="width: 25px;
                 height: 25px;"></span>
                 Женщина
               </label>
@@ -64,53 +118,53 @@
 
             <div class="form-group">
               <label for="">Дата рождения</label>
-              <input type="text" class="form-control" id="date_mask" name="user_birthdate" placeholder="12/02/1995" style="width: 220px;">
+              <input type="text" class="form-control" id="date_mask" name="insurant_birthdate" placeholder="12/02/1995" style="width: 220px;">
             </div>
 
             <legend><h3>Паспортные данные</h3></legend>
 
             <div class="form-group">
               <label for="">Серия и номер паспорта</label>
-              <input type="text" class="form-control" id="psn" name="user_psn" placeholder="XX XX XXXXXX" style="width: 235px;">
+              <input type="text" class="form-control" id="psn" name="insurant_psn" placeholder="XX XX XXXXXX" style="width: 235px;">
             </div>
 
             <div class="form-group">
               <label for="">Кем выдан</label>
-              <textarea name="user_from" class="" id="" name="user_data" rows="3" cols="42" style="font-size: 26px; color: white;"></textarea>
+              <textarea name="user_from" class="" id="" name="issued_by" rows="3" cols="42" style="font-size: 26px; color: white;"></textarea>
             </div>
 
             <div class="form-group">
               <label for="">Дата выдачи</label>
-              <input type="text" class="form-control" id="date_mask2" name="user_date" placeholder="12/02/1995" style="width: 220px;">
+              <input type="text" class="form-control" id="date_mask2" name="date_issue" placeholder="12/02/1995" style="width: 220px;">
             </div>
 
             <legend><h3>Адрес регистрации</h3></legend>
 
             <div class="form-group">
               <label for="">Город или населенный пункт</label>
-              <input type="text" class="form-control" id="" name="user_city" placeholder="Введите город или населенный пункт" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insurant_city_reg" placeholder="Введите город или населенный пункт" style="width: 650px;">
             </div>
 
             <div class="form-group">
               <label for="">Улица</label>
-              <input type="text" class="form-control" id="" name="user_street" placeholder="Введите улицу" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insurant_street_reg" placeholder="Введите улицу" style="width: 650px;">
             </div>
 
             <div class="form-group" style="float: left;">
               <label for="">Дом</label>
-              <input type="text" class="form-control" id="" name="user_house" style="text-align: center;">
+              <input type="text" class="form-control" id="" name="insurant_house_reg" style="text-align: center;">
             </div>
 
             <div class="form-group" style="float: left; margin-left: 50px;">
               <label for="">Квартира</label>
-              <input type="text" class="form-control" id="" name="user_room" style="text-align: center;">
+              <input type="text" class="form-control" id="" name="insurant_room_reg" style="text-align: center;">
             </div>
 
             <legend><h3>Адрес проживания</h3></legend>
 
             <div class="form-group" style="text-align: center;">
               <label for="" >
-                <span><input type="checkbox" class="" id="" name="user_address" style="width: 20px;
+                <span><input type="checkbox" class="" id="" name="insurant_address_res" style="width: 20px;
                 height: 20px;"></span>
                 Адрес проживания совпадает с адресом регистрации
               </label>
@@ -118,56 +172,56 @@
 
             <div class="form-group">
               <label for="">Город или населенный пункт</label>
-              <input type="text" class="form-control" id="" name="user_city" placeholder="Введите город или населенный пункт" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insurant_city_res" placeholder="Введите город или населенный пункт" style="width: 650px;">
             </div>
 
             <div class="form-group">
               <label for="">Улица</label>
-              <input type="text" class="form-control" id="" name="user_street" placeholder="Введите улицу" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insurant_street_res" placeholder="Введите улицу" style="width: 650px;">
             </div>
 
             <div class="form-group" style="float: left;">
               <label for="">Дом</label>
-              <input type="text" class="form-control" id="" name="user_house" style="text-align: center;">
+              <input type="text" class="form-control" id="" name="insurant_house_res" style="text-align: center;">
             </div>
 
             <div class="form-group" style="float: left; margin-left: 50px;">
               <label for="">Квартира</label>
-              <input type="text" class="form-control" id="" name="user_room" style="text-align: center;">
+              <input type="text" class="form-control" id="" name="insurant_room_res" style="text-align: center;">
             </div>
 
             <legend><h1>Застрахованный</h1></legend>
 
             <div class="form-group" style="text-align: center;">
               <label for="" >
-                <span><input type="checkbox" class="" id="" name="user_include"  style="width: 20px; height: 20px;"></span>
+                <span><input type="checkbox" class="" id="" name="insured_include"  style="width: 20px; height: 20px;"></span>
                 Включить страхователя в список застрахованных
               </label>
             </div>
 
             <div class="form-group">
       				<label for="">Фамилия</label>
-      				<input type="text" class="form-control" id="" name="user_lastname" placeholder="Иванов" style="width: 650px;">
+      				<input type="text" class="form-control" id="" name="insured_lastname" placeholder="Иванов" style="width: 650px;">
       			</div>
 
             <div class="form-group">
       				<label for="">Имя</label>
-      				<input type="text" class="form-control" id="" name="user_name" placeholder="Иван" style="width: 650px;">
+      				<input type="text" class="form-control" id="" name="insured_name" placeholder="Иван" style="width: 650px;">
       			</div>
 
             <div class="form-group">
               <label for="">Отчество</label>
-              <input type="text" class="form-control" id="" name="user_middlename" placeholder="Иванович" style="width: 650px;">
+              <input type="text" class="form-control" id="" name="insured_middlename" placeholder="Иванович" style="width: 650px;">
             </div>
 
             <div class="form-group">
               <label for="">Дата рождения</label>
-              <input type="text" class="form-control" id="date_mask3" name="user_birthdate" placeholder="12/02/1995" style="width: 220px;">
+              <input type="text" class="form-control" id="date_mask3" name="insured_birthdate" placeholder="12/02/1995" style="width: 220px;">
             </div>
 
             <hr>
 
-      			<button type="submit" class="btn">Оплатить полис</button>
+      			<button type="submit" class="btn" name="payment">Оплатить полис</button>
 
       		</form>
       	</div>
