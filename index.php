33cdfe45f3591988f918a6b3a291c7aed2b5f68b
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-20 06:13:50 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ require_once('db.php');
     $user_username = $_POST['username'];
     $user_password = $_POST['password'];
     if (!empty($user_username) && !empty($user_password)) {
-      $_SESSION['logged_user'] = $_POST['username'];
+      //$_SESSION['logged_user'] = $_POST['username'];
       $sql_select = "SELECT 'id', 'login' FROM users WHERE login = '$user_username' AND password = '$user_password'";
       $stmt = $conn->query($sql_select);
       $stmt->execute();
       $data = $stmt->fetchAll();
+      $_SESSION['logged_user'] = $data['login'];
     }
     else {
       echo '<div style = "color: red; text-align: center;">Поля заполнены неправельно!</div><hr>';
