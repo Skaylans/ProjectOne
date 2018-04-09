ea418db9fac156b4ec7ea8e04c55034706bd474e
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-04-10 00:00:01 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -2,7 +2,7 @@
 
 try {
     $conn = new PDO($dsn, $login, $pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE insur(
           insurer_id INT NOT NULL IDENTITY(1,1),
           PRIMARY KEY(insurer_id),
